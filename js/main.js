362fe2414e548b8908f7fef0c8921e4ec0b00ef9
danieldauk <danieldauk@gmail.com>
2017-05-29 11:54:37 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var playerChoice;
+var humanPlayer;
+var aiPlayer;
 var choice;
 
 $(function(){
@@ -9,32 +10,65 @@ $(function(){
   function chooseSide() {
      choice = prompt("X or O?");
     if(choice.toUpperCase() == "X") {
-      playerChoice =1;
+      humanPlayer = "X";
+      aiPlayer = "O";
     } else if(choice.toUpperCase() == "O") {
-      playerChoice =2;
+      humanPlayer = "O";
+      aiPlayer = "X";
     } else {
       chooseSide();
     }
   }
   
   
-  //adding X or O on click and preventing from duplicates
+  //on square click 
   $(".square").on("click", function() {
+    
+    //preventing from duplicates and error message
     if($(this).hasClass("cross") || $(this).hasClass("circle")) {
       var temp = this;
      $(this).addClass("error");
-      console.log(temp);
      setTimeout(function(){
        $(temp).removeClass("error");
      }, 300);
-    } else if(playerChoice==1) {
+      
+      //if player chose x than x.svg if "o" than o.svg
+    } else if(humanPlayer=="X") {
       $(this).addClass("cross");
       $(this).find("img").attr("src", "img/x.svg");
+      //putting moves into firstBoard array
+      firstBoard[$(this).attr("value")] = humanPlayer;
     } else {
       $(this).addClass("circle");
       $(this).find("img").attr("src", "img/o.svg");
+      //putting moves into firstBoard array
+      firstBoard[$(this).attr("value")] = humanPlayer;
     }
   });
+  
+  //defining gaming firstBoard
+  var firstBoard = [0,1,2,
+                    3,4,5,
+                    6,7,8];
+  
+  //checking for winning combos
+  function checkForWin(board, player) {
+    
+    if((board[0] == player && board[1] == player && board[2] == player) ||
+       (board[3] == player && board[4] == player && board[5] == player) ||
+       (board[6] == player && board[7] == player && board[8] == player) ||
+       (board[0] == player && board[3] == player && board[6] == player) ||
+       (board[1] == player && board[4] == player && board[7] == player) ||
+       (board[2] == player && board[5] == player && board[8] == player) ||
+       (board[0] == player && board[4] == player && board[8] == player) ||
+       (board[2] == player && board[4] == player && board[6] == player)) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+
   
   
   
